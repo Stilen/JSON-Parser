63866f40ab69f505708a8a9bfc09e13a6d0929db
Pedro Silva <Pedro Silva>
2016-08-05 15:13:50 +0100

    Commit one

---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Main
+{
+  static String url = "http://api.goeuro.com/api/v2/position/suggest/en/";
+  static String welcome = "This app returns the ID, name, type, latitude and longitude of one city.";
+  static String input;
+  
+  public static void main(String[] args)
+    throws IOException, JSONException
+  {
+    Scanner in = new Scanner(System.in);
+    System.out.println(welcome);
+    String s;
+    do
+    {
+      System.out.println("Please insert the name of the city below.");
+      getValidInput(in);
+      JSONArray json = readJsonFromUrl(url + input);
+      while (json.length() == 0)
+      {
+        System.out.println("There is no information about " + input + ", please insert another name.");
+        getValidInput(in);
+        json = readJsonFromUrl(url + input);
+      }
+      printer(json, input);
+      s = in.next();
+    } while (s.equals("Y"));
+    in.close();
+  }
+  
+  public static boolean isWord(String s)
+  {
+    char[] chars = s.toCharArray();
+    char[] arrayOfChar1;
+    int j = (arrayOfChar1 = chars).length;
+    for (int i = 0; i < j; i++)
+    {
+      char c = arrayOfChar1[i];
+      if (!Character.isLetter(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  public static void getValidInput(Scanner in)
+  {
+    input = in.next();
+    Boolean state = Boolean.valueOf(false);
+    while (!state.booleanValue())
+    {
+      state = Boolean.valueOf(isWord(input));
+      if (!state.booleanValue())
+      {
+        System.out.println("This input is not valid, please insert a new one.");
+        input = in.next();
+      }
+    }
+  }
+  
+  private static String readAll(Reader rd)
+    throws IOException
+  {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1)
+    {
+      sb.append((char)cp);
+    }
+    return sb.toString();
+  }
+  
+  public static JSONArray readJsonFromUrl(String url)
+    throws IOException, JSONException
+  {
+    InputStream is = new URL(url).openStream();
+    try
+    {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONArray json = new JSONArray(jsonText);
+      return json;
+    }
+    finally
+    {
+      is.close();
+    }
+  }
+  
+  public static void printer(JSONArray array, String input)
+    throws JSONException, IOException
+  {
+    FileWriter fw = new FileWriter(input + ".csv");
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < array.length(); i++)
+    {
+      JSONObject obj = array.getJSONObject(i);
+      sb.append(obj.get("_id"));
+      sb.append(",");
+      sb.append(obj.get("name"));
+      sb.append(",");
+      sb.append(obj.get("type"));
+      sb.append(",");
+      
+      JSONObject geo = (JSONObject)obj.get("geo_position");
+      sb.append(geo.get("latitude"));
+      sb.append(",");
+      sb.append(geo.get("longitude"));
+      sb.append("\n");
+    }
+    fw.write(sb.toString());
+    fw.close();
+    System.out.println("Your query has been saved!\nDo you wish to continue?(Y/n)");
+  }
+}
